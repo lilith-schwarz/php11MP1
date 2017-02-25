693be1e8785c48b2c4a07db3c543b0ff988ff67b
lilith-schwarz <Eli@Elisabeths-MBP.fritz.box>
2017-02-25 20:54:32 +0100

    psw gehashed, code kommentiert

--- admin.php
+++ admin.php
@@ -8,6 +8,7 @@
 	<link href="stylesheet.css" rel="stylesheet">
 </head>
 <body>
+<!-- Header -->
 	<div id="headeradmin">
 		<img src="images/logo_loca_mole.png" id="logo"/>
 	</div>
@@ -22,8 +23,9 @@
 <?php
 session_start();
 $verbindung="";
-$verbindung = getDb($verbindung);
+$verbindung=getDb();	//Stellt eine Datenbank-Verbindung her
 
+// Values für Formular werden initialisiert
 $button="Hinzuf&uuml;gen";
 $varname="";
 $varpreis="";
@@ -34,87 +36,160 @@ $varbeschreibung="";
 $varid="";
 
 
-if (isset($_POST['loschen'])){
-	$sql="DELETE FROM produkte WHERE id = '{$_POST['loschen']}'";
+if (isset($_POST['loschen'])){	//wenn der Button "Löschen" geklickt wurde
+	button_loeschen();
+}
+else if (isset($_POST['bearbeiten'])){	//wenn der Button "Bearbeiten" geklickt wurde
+	button_bearbeiten();
+}
+
+create_editform(); //Erstellt das Formular zum Hinzufügen/Bearbeiten der Einträge
+
+if (!isset($_SESSION['visited'])) { //Wenn nicht eingeloggt (Session inaktiv), dann einloggen
+	login();	
+} 
+
+if(isset($_POST["Hinzufügen"])){	//Button "Hinzufügen" wurde gedrückt
+	add_item();
+}
+else if(isset($_POST["Ändern"])){ 	//Button "Ändern" wurde gedrückt
+	edit_item();	
+}
+
+show_itemlist();	//zeigt alle Datenbankeinträge an
+
+mysqli_close($verbindung);	//schließt die Datenbank-Verbindung
+
+
+function getDb(){
+	global $verbindung;	//Variablen müssen als global deklariert werden, damit man in der Funktion Zugriff hat
+
+	if($verbindung==""){	//Wenn es noch keine Verbindung gibt...
+		try{				//...versuche eine herzustellen
+			$a= @mysqli_connect("localhost", "root", "");	
+			mysqli_select_db($a, "locamole");
+			return $a;		//gib die Datenbank zurück an $verbindung
+
+		}catch(mysqli_error $e){	//Fehlerhandling
+			echo "Fehler: ".$e($a);
+		}
+	}else{
+		return $verbindung;	//Wenn schon eine Verbindung existiert, gib sie einfach zurück
+	}
+}
+
+function button_loeschen(){
+
+	global $verbindung;
+
+	$sql="DELETE FROM produkte WHERE id = '{$_POST['loschen']}'";	//Lösche das Item mit der ID xy
 	mysqli_query($verbindung, $sql);
+}
 
-}else if (isset($_POST['bearbeiten'])){
-	$button="&Auml;ndern";
-	$sql="SELECT * FROM produkte WHERE id='{$_POST['bearbeiten']}'";
+function button_bearbeiten(){
+
+	global $button;
+	global $varname;
+	global $varpreis;
+	global $varbild;
+	global $varmenge;
+	global $varzutaten;
+	global $varbeschreibung;
+	global $varid;
+	global $verbindung;
+
+	$button="&Auml;ndern";		//Umbennenung des Buttons
+	$sql="SELECT * FROM produkte WHERE id='{$_POST['bearbeiten']}'";	//Gib mir alle Infos des items mit der id xy
 	$abfrage=mysqli_query($verbindung, $sql);
-
 	$infos = mysqli_fetch_assoc($abfrage);
-	$varname= $infos['name'];
+
+	$varname= $infos['name'];			//Ändern Textfeld-Values (werden vorausgefüllt)
 	$varpreis= $infos['preis'];
 	$varbild= $infos['bild'];
 	$varmenge= $infos['menge'];
 	$varzutaten= $infos['zutaten'];
 	$varbeschreibung= $infos['beschreibung'];
 	$varid = $infos['id'];
+
+	mysqli_free_result($abfrage);	//gibt den annektierten Speicher frei
 }
 
+function create_editform(){	//generiert die Eingabe-Felder (bei Bearbeiten vorausgefüllt)
 
-echo '<form class="dazu" method="post" action="admin.php">';
-      
-echo '<div class="formdazu">';
-echo '<input type="text" name="name" value="'.$varname.'" placeholder="Produktname"/>';
-echo '<input type="text" name="preis" value="'.$varpreis.'" placeholder="Preis"/>';
-echo '<input type="text" name="bild" value="'.$varbild.'" placeholder="Bild-URL"/>';
-echo '<input type="text" name="menge" value="'.$varmenge.'" placeholder="Menge"/>';
-echo '<input type="text" name="zutaten" value="'.$varzutaten.'" placeholder="Zutaten"/>';
-echo '<input type="text" name="beschreibung" value="'.$varbeschreibung.'" placeholder="Beschreibung"/>';
-echo '<button type="submit" name="'.$button.'"" value="'.$varid.'" id="dazu">'.$button.'</button>';
-echo '</div>';
-echo '</form>';
+	global $button;
+	global $varname;
+	global $varpreis;
+	global $varbild;
+	global $varmenge;
+	global $varzutaten;
+	global $varbeschreibung;
+	global $varid;
 
+	echo '<form class="dazu" method="post" action="admin.php">';    
+	echo '<div class="formdazu">';
+	echo '<input type="text" name="name" value="'.$varname.'" placeholder="Produktname"/>';
+	echo '<input type="text" name="preis" value="'.$varpreis.'" placeholder="Preis"/>';
+	echo '<input type="text" name="bild" value="'.$varbild.'" placeholder="Bild-URL"/>';
+	echo '<input type="text" name="menge" value="'.$varmenge.'" placeholder="Menge"/>';
+	echo '<input type="text" name="zutaten" value="'.$varzutaten.'" placeholder="Zutaten"/>';
+	echo '<input type="text" name="beschreibung" value="'.$varbeschreibung.'" placeholder="Beschreibung"/>';
+	echo '<button type="submit" name="'.$button.'"" value="'.$varid.'" id="dazu">'.$button.'</button>';
+	echo '</div>';
+	echo '</form>';
+}
 
+function login(){
 
+	global $verbindung;
 
-if (!isset($_SESSION['visited'])) {
-   //echo "Du hast diese Seite noch nicht besucht";
-	    $query="SELECT * FROM login";
-		$abfrage=mysqli_query($verbindung, $query);
-		$login=mysqli_fetch_assoc($abfrage);
+    $query="SELECT * FROM login";	//holt sich die Login-Daten von der DB
+	$abfrage=mysqli_query($verbindung, $query);
+	$login=mysqli_fetch_assoc($abfrage);
 
-	if ($_POST['psw']!=$login['password'] && $_POST['user']!=$login['user']) {
+	$hashed_psw=psw_hash($_POST['psw']);	//verschlüsselt das eingegebene psw (steht in der DB auch verschlüsselt)
+
+	if ($hashed_psw==$login['password'] && $_POST['user']==$login['user']) { 	//vergleicht die Eingabe mit den Daten in der DB
+		   $_SESSION['visited'] = true;											// wenn login erfolgreich, Session aktiv
+	}
+	else{
 		echo "<script language=\"JavaScript\">
 		<!--
 		alert(\"Bitte überprüfen sie ihre Daten!\");
 		//-->
-		location.href = 'login.php';
-		</script>";
-		// else $_SESSION["loginok"] = true;
-	}else{
-		   $_SESSION['visited'] = true;
-
+		location.href = 'login.php';	
+		</script>";						//User wird bei falscher Eingabe zurück auf die Login-Seite geschickt
 	}
-} else {
-   //echo "Du hast diese Seite zuvor schon aufgerufen";
+	mysqli_free_result($abfrage);
 }
 
+function add_item(){	//Hinzufügen von DB-Einträgen
+	global $verbindung;
 
-//wenn alles ausgefüllt ist, füge es in die Datenbank ein
-
-if(!(isset($_POST["Ändern"]))){
+	//Wenn keines der Eingabe-Felder leer ist..
 	if(!(empty($_POST["name"])) && !(empty($_POST["preis"])) && !(empty($_POST["beschreibung"])) && !(empty($_POST["bild"])) && !(empty($_POST["zutaten"])) && !(empty($_POST["menge"]))){
-
+			//...füge das Item der Datenbank hinzu
 			$sql="INSERT INTO produkte (name, preis, beschreibung, bild, zutaten, menge) VALUES ('{$_POST["name"]}', '{$_POST["preis"]}', '{$_POST["beschreibung"]}', '{$_POST["bild"]}', '{$_POST["zutaten"]}', '{$_POST["menge"]}')";
 			mysqli_query($verbindung, $sql);
 
-	}else if (isset($_POST['Hinzufügen'])){
+	}else if (isset($_POST['Hinzufügen'])){		//Wenn "Hinzufügen" geklickt wurde, aber ein Feld noch leer ist
 	echo "<script language=\"JavaScript\">
 			<!--
 			alert(\"Bitte alle Felder ausfüllen!\");
 			//-->
-			</script>";
-	} }
-if(isset($_POST["Ändern"])){		
+			</script>";		//Fehlermeldung
+	} 
+}
+
+function edit_item(){	//Bearbeiten von DB-Einträgen
+	global $verbindung;
+	
+	//Alle Felder müssen ausgefüllt sein
 	if(!(empty($_POST["name"])) && !(empty($_POST["preis"])) && !(empty($_POST["beschreibung"])) && !(empty($_POST["bild"])) && !(empty($_POST["zutaten"])) && !(empty($_POST["menge"]))){
 
+			//Item wird geupdatet
 			$sql="UPDATE produkte SET name='{$_POST["name"]}', preis='{$_POST["preis"]}', beschreibung='{$_POST["beschreibung"]}', bild='{$_POST["bild"]}', zutaten='{$_POST["zutaten"]}', menge='{$_POST["menge"]}' WHERE id='{$_POST["Ändern"]}'";
 			mysqli_query($verbindung, $sql);
 		}
-
 		else if (isset($_POST['Ändern'])){
 			echo "<script language=\"JavaScript\">
 					<!--
@@ -124,47 +199,39 @@ if(isset($_POST["Ändern"])){
 	}
 }
 
+function show_itemlist(){	//Zeigt alle Items der DB an
+	global $verbindung;
 
-$sql="SELECT * FROM produkte ORDER BY id";
-$abfrage=mysqli_query($verbindung, $sql);
+	$sql="SELECT * FROM produkte ORDER BY id";
+	$abfrage=mysqli_query($verbindung, $sql);
 
-echo'<table class="katalog">';
-		
-while ($produkte = mysqli_fetch_assoc($abfrage)){
+	echo'<table class="katalog">';
+			
+	while ($produkte = mysqli_fetch_assoc($abfrage)){
 
-	echo"<tr>";
-	echo "<td> <img src='{$produkte ['bild']}' class='bild'/></td>";
-	echo "<td> {$produkte ['id']}</td>";
-	echo "<td> {$produkte ['name']}</td>";
-	echo "<td> {$produkte ['preis']}";
-	echo "<td> {$produkte ['beschreibung']}";
-	echo "<td class='leer'><form class='dazu' method='post' action='admin.php'>";
-	echo "<button type='submit' name='bearbeiten' value='{$produkte['id']}' id='dazu'>Bearbeiten</button>";
-    echo "<button type='submit' name='loschen' value='{$produkte['id']}' id='dazu'>L&ouml;schen</button>";
-    echo "</form></td>";
-	echo "</tr>";
-
-}
-
-echo'</table>';
-
-function getDb($db_verbindung){
-	if($db_verbindung==""){
-		try{
-			$a= @mysqli_connect("localhost", "root", "");
-			mysqli_select_db($a, "locamole");
-			return $a;
-
-		}catch(mysqli_error $e){
-			echo "Fehler: ".$e($a);
-		}
-	}else{
-		return $db_verbindung;
+		echo"<tr>";
+		echo "<td> <img src='{$produkte ['bild']}' class='bild'/></td>";
+		echo "<td> {$produkte ['id']}</td>";
+		echo "<td> {$produkte ['name']}</td>";
+		echo "<td> {$produkte ['preis']}";
+		echo "<td> {$produkte ['beschreibung']}";
+		echo "<td class='leer'><form class='dazu' method='post' action='admin.php'>";
+		echo "<button type='submit' name='bearbeiten' value='{$produkte['id']}' id='dazu'>Bearbeiten</button>";
+	    echo "<button type='submit' name='loschen' value='{$produkte['id']}' id='dazu'>L&ouml;schen</button>";
+	    echo "</form></td>";
+		echo "</tr>";
 	}
+	echo'</table>';
+	mysqli_free_result($abfrage);
 }
 
-mysqli_free_result($abfrage);
-mysqli_close($verbindung);
+function psw_hash($Input) {
+  $Input=iconv('UTF-8','UTF-16LE',$Input);	 //konvertivert das psw von UTF8 zu UTF16
+  $MD4Hash=bin2hex(mhash(MHASH_MD4,$Input)); //Verschlüsselt mit MD4 hash
+  $upperHash=strtoupper($MD4Hash);	//alles in Großbuchstaben
+
+  return($upperHash);
+}
 
 ?>
 </body>
