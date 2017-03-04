1903e91871a6ada0f3934294308128a2d7a5b177
lilith-schwarz <Eli@Elisabeths-MBP.fritz.box>
2017-03-04 12:38:32 +0100

    search-functions EVERYWHERE

--- admin.php
+++ admin.php
@@ -18,8 +18,14 @@
 	<div id="menue">
 		<span class="menue"> <a href="katalog.php" class="index">Produkte</a></span>
 		<span class="bullet"> &bull;</span>
-		<span class="menue"><a href="impressum.html" class="index">Impressum</a></span>
-</div>
+		<span class="menue"><a href="impressum.html" class="index">Impressum</a></span></div>
+		<div><form method="post" action="admin.php" id="searchform">
+		<input type="text" name="searchString" placeholder="Suche">
+		<button type="submit" name="searchButton">Suche</button>
+		</form>
+		</div>
+
+
 
 <?php
 session_start();
@@ -36,6 +42,9 @@ $varzutaten="";
 $varbeschreibung="";
 $varid="";
 
+if (!isset($_SESSION['visited'])) { //Wenn nicht eingeloggt (Session inaktiv), dann einloggen
+	login();	
+} 
 
 if (isset($_POST['loschen'])){	//wenn der Button "Löschen" geklickt wurde
 	button_loeschen();
@@ -46,10 +55,6 @@ else if (isset($_POST['bearbeiten'])){	//wenn der Button "Bearbeiten" geklickt w
 
 create_editform(); //Erstellt das Formular zum Hinzufügen/Bearbeiten der Einträge
 
-if (!isset($_SESSION['visited'])) { //Wenn nicht eingeloggt (Session inaktiv), dann einloggen
-	login();	
-} 
-
 if(isset($_POST["Hinzufügen"])){	//Button "Hinzufügen" wurde gedrückt
 	add_item();
 }
@@ -57,7 +62,11 @@ else if(isset($_POST["Ändern"])){ 	//Button "Ändern" wurde gedrückt
 	edit_item();	
 }
 
-show_itemlist();	//zeigt alle Datenbankeinträge an
+if(isset($_POST['searchButton'])) {
+	searchAndShow_itemlist();	//zeige Suchergebnisse an (Wenn "Suche" gedrückt wurde)
+}else {
+	show_itemlist();	//...oder zeige alle Datenbankeinträge an
+}
 
 mysqli_close($verbindung);	//schließt die Datenbank-Verbindung
 
@@ -137,6 +146,8 @@ function create_editform(){	//generiert die Eingabe-Felder (bei Bearbeiten vorau
 	echo '<button type="submit" name="'.$button.'"" value="'.$varid.'" id="dazu" class="button">'.$button.'</button>';
 	echo '</div>';
 	echo '</form>';
+	echo '<hr>';
+
 }
 
 function login(){
@@ -148,7 +159,7 @@ function login(){
 	$login=mysqli_fetch_assoc($abfrage);
 
 	$hashed_psw=psw_hash($_POST['psw']);	//verschlüsselt das eingegebene psw (steht in der DB auch verschlüsselt)
-
+	
 	if ($hashed_psw==$login['password'] && $_POST['user']==$login['user']) { 	//vergleicht die Eingabe mit den Daten in der DB
 		   $_SESSION['visited'] = true;											// wenn login erfolgreich, Session aktiv
 	}
@@ -214,9 +225,9 @@ function show_itemlist(){	//Zeigt alle Items der DB an
 		echo "<td id='tbild'> <img src='{$produkte ['bild']}' class='bild'/></td>";
 		echo "<td><h2 id='th2'> {$produkte ['name']}</h2></td>";
 		echo "<td id='tp'> {$produkte ['preis']} &euro; </td>";
-		echo "<td id='tm'> {$produkte ['menge']} &euro; </td>";
+		echo "<td id='tm'> {$produkte ['menge']}</td>";
 		echo "<td id='tb'> {$produkte ['beschreibung']} </td>";
-		echo "<td id='tz'> {$produkte ['zutaten']}</td>";
+		echo "<td id='tz'><strong>Zutaten: </strong> {$produkte ['zutaten']}</td>";
 		echo "<td class='leer'><form class='dazu' method='post' action='admin.php'>";
 		echo "<button type='submit' name='bearbeiten' value='{$produkte['id']}' class='button' id='ed'>Bearbeiten</button><br/>";
 	    echo "<button type='submit' name='loschen' value='{$produkte['id']}' onclick=\"return confirm('Wirklich löschen?')\" class='button' id='del'>L&ouml;schen</button></form></td>";
@@ -234,6 +245,50 @@ function psw_hash($Input) {
 
   return($upperHash);
 }
+
+function searchAndShow_itemlist(){
+	global $verbindung;
+
+	$insertString=$_POST['searchString'];
+	$insertArray=explode(" ", $insertString);		//Teilt Suchstring in einzelne Begriffe nach Leerzeichen
+	$wordCount=count($insertArray);					//Zählt, wieviele einzelne Suchbegriffe es gibt
+
+	$sql = "SELECT * FROM produkte WHERE ";			//SQL Befehl wird in for-Schleife fertig definiert		
+
+	for ($i=0; $i < $wordCount; $i++) { 			
+
+		if($i==0){		//Wenn es der 1. Suchbegriff ist
+			$sql .= "name LIKE '%".$insertArray[$i]."%' OR zutaten LIKE '%".$insertArray[$i]."%'";	
+
+		}else{			//Für alle weiteren muss ein OR davor...
+			$sql.=" OR name LIKE '%".$insertArray[$i]."%' OR zutaten LIKE '%".$insertArray[$i]."%'";
+
+		}
+	}
+
+	$abfrage=mysqli_query($verbindung, $sql);
+
+	echo'<table class="katalog">';
+
+	while ($produkte = mysqli_fetch_assoc($abfrage)){
+
+		echo"<tr>";
+		echo "<td id='tbild'> <img src='{$produkte ['bild']}' class='bild'/></td>";
+		echo "<td><h2 id='th2'> {$produkte ['name']}</h2></td>";
+		echo "<td id='tp'> {$produkte ['preis']} &euro; </td>";
+		echo "<td id='tm'> {$produkte ['menge']}</td>";
+		echo "<td id='tb'> {$produkte ['beschreibung']} </td>";
+		echo "<td id='tz'><strong>Zutaten: </strong> {$produkte ['zutaten']}</td>";
+		echo "<td class='leer'><form class='dazu' method='post' action='admin.php'>";
+		echo "<button type='submit' name='bearbeiten' value='{$produkte['id']}' class='button' id='ed'>Bearbeiten</button><br/>";
+	    echo "<button type='submit' name='loschen' value='{$produkte['id']}' onclick=\"return confirm('Wirklich löschen?')\" class='button' id='del'>L&ouml;schen</button></form></td>";
+		echo "</tr>";
+	}
+
+	echo'</table>';
+	mysqli_free_result($abfrage);
+}
+
 
 ?>
 <footer>
--- katalog.php
+++ katalog.php
@@ -17,6 +17,11 @@
 <span class="bullet"> &bull;</span>
 <span class="menue"><a href="impressum.html" class="index">Impressum</a></span>
 </div>
+<div><form method="post" action="katalog.php" id="searchform">
+		<input type="text" name="searchString" placeholder="Suche">
+		<button type="submit" name="searchButton">Suche</button>
+		</form>
+		</div>
 
 <?php
 session_start();
@@ -35,28 +40,78 @@ try{				//versuche eine Verbindung herzustellen
 	echo "Fehler: ".$e($a);
 }
 
-$sql="SELECT * FROM produkte ORDER BY id";
-$abfrage=mysqli_query($verbindung, $sql);
-
-echo'<table class="katalog">';
-
-while ($produkte = mysqli_fetch_assoc($abfrage)){
-	echo"<tr>";
-	echo "<td id='tbild'> <img src='{$produkte ['bild']}' class='bild'/></td>";
-	echo "<td><h2 id='th2'> {$produkte ['name']}</h2></td>";
-	echo "<td id='tp'> {$produkte ['preis']} &euro; </td>";
-	echo "<td id='tm'> {$produkte ['menge']} &euro; </td>";
-	echo "<td id='tb'> {$produkte ['beschreibung']} </td>";
-	echo "<td id='tz'> {$produkte ['zutaten']}</td>";
-	echo "<td class='leer'></td>";
-	echo"</tr>";
+if(isset($_POST['searchButton'])) {
+	searchAndShow_itemlist();	//zeige Suchergebnisse an (Wenn "Suche" gedrückt wurde)
+}else {
+	show_itemlist();	//...oder zeige alle Datenbankeinträge an
 }
 
-echo'</table>';
+function show_itemlist(){	//Zeigt alle Items der DB an
+	global $verbindung;
 
-mysqli_free_result($abfrage);
+	$sql="SELECT * FROM produkte ORDER BY id";
+	$abfrage=mysqli_query($verbindung, $sql);
+
+	echo'<table class="katalog">';
+
+	while ($produkte = mysqli_fetch_assoc($abfrage)){
+
+		echo"<tr>";
+		echo "<td id='tbild'> <img src='{$produkte ['bild']}' class='bild'/></td>";
+		echo "<td><h2 id='th2'> {$produkte ['name']}</h2></td>";
+		echo "<td id='tp'> {$produkte ['preis']} &euro; </td>";
+		echo "<td id='tm'> {$produkte ['menge']}</td>";
+		echo "<td id='tb'> {$produkte ['beschreibung']} </td>";
+		echo "<td id='tz'><strong>Zutaten: </strong> {$produkte ['zutaten']}</td>";
+		echo "<td class='leer'></td>";
+		echo "</tr>";
+	}
+
+	echo'</table>';
+	mysqli_free_result($abfrage);
+}
+
+function searchAndShow_itemlist(){
+	global $verbindung;
+
+	$insertString=$_POST['searchString'];
+	$insertArray=explode(" ", $insertString);		//Teilt Suchstring in einzelne Begriffe nach Leerzeichen
+	$wordCount=count($insertArray);					//Zählt, wieviele einzelne Suchbegriffe es gibt
+
+	$sql = "SELECT * FROM produkte WHERE ";			//SQL Befehl wird in for-Schleife fertig definiert		
+
+	for ($i=0; $i < $wordCount; $i++) { 			
+
+		if($i==0){		//Wenn es der 1. Suchbegriff ist
+			$sql .= "name LIKE '%".$insertArray[$i]."%' OR zutaten LIKE '%".$insertArray[$i]."%'";	
+
+		}else{			//Für alle weiteren muss ein OR davor...
+			$sql.=" OR name LIKE '%".$insertArray[$i]."%' OR zutaten LIKE '%".$insertArray[$i]."%'";
+
+		}
+	}
+	$abfrage=mysqli_query($verbindung, $sql);
+
+	echo'<table class="katalog">';
+
+	while ($produkte = mysqli_fetch_assoc($abfrage)){
+
+		echo"<tr>";
+		echo "<td id='tbild'> <img src='{$produkte ['bild']}' class='bild'/></td>";
+		echo "<td><h2 id='th2'> {$produkte ['name']}</h2></td>";
+		echo "<td id='tp'> {$produkte ['preis']} &euro; </td>";
+		echo "<td id='tm'> {$produkte ['menge']}</td>";
+		echo "<td id='tb'> {$produkte ['beschreibung']} </td>";
+		echo "<td id='tz'><strong>Zutaten: </strong> {$produkte ['zutaten']}</td>";
+		echo "<td class='leer'></td>";
+		echo "</tr>";
+	}
+
+	echo'</table>';
+	mysqli_free_result($abfrage);
+}
+
 mysqli_close($verbindung);
-
 ?>
 <footer>
   <span class="footer"> <a href="login.php" class="index">Admin-Login</a></span>
